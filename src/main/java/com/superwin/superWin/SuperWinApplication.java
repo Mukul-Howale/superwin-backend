32a54b52f5e9d1cd2a55eadc593d7c05f5f438c3
Mukul Howale <howalemukul@gmail.com>
2025-02-14 01:07:53 +0530

    Basic files

---
+++ src/main/java/com/superwin/superWin/SuperWinApplication.java
@@ -0,0 +1,13 @@
+package com.superwin.superWin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperWinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperWinApplication.class, args);
+	}
+
+}
