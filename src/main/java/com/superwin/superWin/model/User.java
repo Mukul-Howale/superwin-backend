8f277bd12b20098fe6815512022ad2896b5484f6
Mukul Howale <howalemukul@gmail.com>
2025-02-15 01:58:46 +0530

    Added User model

---
+++ src/main/java/com/superwin/superWin/model/User.java
@@ -0,0 +1,37 @@
+package com.superwin.superWin.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String phoneNumber;
+    private String userName;
+    private String password;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
